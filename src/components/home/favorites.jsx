60c02514a86059dfede7083295b2678cbece0432
NerDev-Bug <jhonalfietorzar0987@gmail.com>
2026-01-05 15:44:17 +0800

    feat: add Favorites component with background image to Home page

--- src/components/home/favorites.jsx
+++ src/components/home/favorites.jsx
@@ -1,8 +1,8 @@
 function Favorites() {
     return (
-        <div>
-            <div>
-                <h1 className="text-2xl font-bold text-red-600">Favorites</h1>
+        <div className="bg-cover bg-center" style={{ backgroundImage: `url('./images/gingham_pattern_purple_bg.jpg')` }}>
+            <div className="py-8 px-4">
+                <h1 className="text-4xl font-bold text-[#502455] font-cooper text-center">Favorites</h1>
             </div>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
+import Favorites from "../components/home/favorites";
+
 function Home() {
   return (
-    <section className="relative min-h-screen flex items-center overflow-hidden">
-      <div className="max-w-9xl mx-auto w-full px-2">
-        <div className="flex flex-col md:flex-row justify-center items-center">
-          
-        </div>
-      </div>
-    </section>
+    <>
+        <section className="relative min-h-screen flex items-center overflow-hidden">
+          <div className="max-w-9xl mx-auto w-full px-2">
+              <div className="flex flex-col md:flex-row justify-center items-center">
+                  <div className="flex items-center justify-center h-screen">
+                      <h1 className="text-2xl font-bold text-purple-600">Home</h1>
+                  </div>
+              </div>
+          </div>
+      </section>
+      <Favorites />
+    </>
   );
 }
 
