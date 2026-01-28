ded9f5b148d8489501ec7a73ece4687a3541c5d6
NerDev-Bug <jhonalfietorzar0987@gmail.com>
2026-01-28 08:43:52 +0800

    refactor: Cart, CartContext, OrderConfirmation, CheckOutModal, orders

--- src/components/modals/CheckOutModal.jsx
+++ src/components/modals/CheckOutModal.jsx
@@ -67,7 +67,7 @@ function CheckOutModal({
         <div className="fixed inset-0 bg-black/40 z-50" />
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div className="bg-white rounded-xl p-6 shadow-lg">
-            Loading checkout...
+            Loading checkout..
           </div>
         </div>
       </>
